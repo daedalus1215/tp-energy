2f3de8ca6542e4efd1f63cb273deaa19def96952
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-07 23:40:13 -0400

    Add more to constants

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,12 @@ const REALTIME_UPDATE_EVENT = 'emeter-realtime-update';
 const TOTAL_PLUGS_ON_STRIP = 6;
 const IS_CONSOLE_LOGGING = true;
 const VERBOSE = false;
+const INTERVAL = 30000;
 
 module.exports = {
     REALTIME_UPDATE_EVENT,
     TOTAL_PLUGS_ON_STRIP,
     IS_CONSOLE_LOGGING,
-    VERBOSE
+    VERBOSE,
+    INTERVAL
 };
