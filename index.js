618a7cc94ff306551d2cb8a32508afc3e3c865ef
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-07 23:41:18 -0400

    Refactor index (cleaner)

--- index.js
+++ index.js
@@ -3,54 +3,62 @@ const { Client } = require('tplink-smarthome-api');
 const deviceWrite = require('./src/writers/deviceWrite');
 const powerStrip = require('./src/plugs/powerStrip');
 const laptopPlug = require('./src/plugs/laptopPlug');
-const { POLL_INTERVAL, IS_CONSOLE_LOGGING } = require('./src/constants');
+const { VERBOSE, IS_CONSOLE_LOGGING, INTERVAL } = require('./src/constants');
 
 const client = new Client({
     defaultSendOptions: { timeout: 20000, transport: 'tcp' },
 });
 
 const run = async () => {
+    const {
+        POWER_STRIP_IP_ADDRESS,
+        FILE_PATH,
+        BAGGINS_POWER,
+        LAPTOP_POWER,
+    } = process.env;
 
     // Laptop
-    laptopPlug(client, process.env.LAPTOP_POWER)
+    laptopPlug(client, LAPTOP_POWER, INTERVAL, FILE_PATH, VERBOSE, IS_CONSOLE_LOGGING)
 
     // Baggins
-    client.getDevice({ host: process.env.BAGGINS_POWER })
+    client.getDevice({ host: BAGGINS_POWER })
         .then(device => {
             device.on('emeter-realtime-update', (emeterRealtime) => {
-                const electricityData = {
-                    id: device.id,
-                    time: Date.now(),
-                    date: new Date(),
-                    name: `${device.alias} - 2 Mining Rigs`,
-                    model: device.model,
-                    host: process.env.BAGGINS_POWER,
-                    watts: emeterRealtime.power
-                };
-
-                deviceWrite(electricityData, IS_CONSOLE_LOGGING);
+                deviceWrite(
+                    {
+                        id: device.id,
+                        time: Date.now(),
+                        date: new Date(),
+                        name: `${device.alias} - 2 Mining Rigs`,
+                        model: device.model,
+                        host: BAGGINS_POWER,
+                        watts: emeterRealtime.power
+                    },
+                    IS_CONSOLE_LOGGING,
+                    VERBOSE
+                );
             });
-
-            device.startPolling(POLL_INTERVAL);
+            device.startPolling(INTERVAL);
         })
         .catch(e => {
-            const electricityData = {
-                id: device?.id,
-                time: Date.now(),
-                date: new Date(),
-                name: `${device?.alias} - 2 Mining Rigs`,
-                model: device?.model,
-                host: process.env.BAGGINS_POWER,
-                watts: false,
-                description: 'issue with connecting to Baggins'
-            };
-
-            deviceWrite(electricityData, IS_CONSOLE_LOGGING);
+            deviceWrite(
+                {
+                    id: 'no device id because of an error',
+                    time: Date.now(),
+                    date: new Date(),
+                    name: `${device?.alias} - 2 Mining Rigs`,
+                    model: device?.model,
+                    host: BAGGINS_POWER,
+                    watts: false,
+                    description: 'Issue with connecting to Baggins'
+                },
+                IS_CONSOLE_LOGGING,
+                VERBOSE
+            );
         });
 
     // Power Strip
-    powerStrip(client, process.env.POWER_STRIP_IP_ADDRESS)
-
+    powerStrip(client, POWER_STRIP_IP_ADDRESS, INTERVAL, FILE_PATH, VERBOSE, IS_CONSOLE_LOGGING)
 }
 
 
